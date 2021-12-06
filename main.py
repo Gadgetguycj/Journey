735dc8d4566ce7d00482581bd8df8a977d697d5f
Gadgetguycj <chase@gadget.sh>
2021-12-06 05:38:25 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+# https://testdriven.io/blog/fastapi-jwt-auth
+# https://fastapi.tiangolo.com/tutorial/sql-databases/#create-your-fastapi-path-operations
+# https://testdriven.io/blog/fastapi-mongo/#mongodb
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("app.api:app", host="0.0.0.0", port=80, reload=True)
