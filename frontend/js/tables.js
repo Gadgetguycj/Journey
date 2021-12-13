7be0ecedfbf890661976a184f575b6d511402488
dfinn417 <dfinn02@nyit.edu>
2021-12-13 08:58:48 -0500

    please

--- frontend/js/tables.js
+++ frontend/js/tables.js
@@ -1,8 +1,11 @@
+var curPage = 0;
+
 window.onload = function addFoodToLog() {
   var table = document.getElementById("food_log");
   var dataStore;
+
     $.ajax({
-        "url": "http://journey.gadget.sh:8080/food",
+        "url": "http://journey.gadget.sh:8080/food?skip="+curPage+"&limit=100",
         "method": "GET",
         "timeout": 0,
         "async": false,
@@ -51,6 +54,48 @@ window.onload = function addFoodToLog() {
 //        }
 //    });
 //}
+
+function changeFoodLogPage(const num) {
+  var table = document.getElementById("food_log");
+  var dataStore;
+    
+  if(num == 1){
+      curPage = curPage + 1
+  }
+  else{
+      curPage = curPage - 1
+  }
+    $.ajax({
+        "url": "http://journey.gadget.sh:8080/food?skip="+curPage+"&limit=100",
+        "method": "GET",
+        "timeout": 0,
+        "async": false,
+        "headers": {
+        "Authorization": "Bearer "+localStorage.getItem("token")
+        },
+        "success": function(data){
+            dataStore = data;
+            console.log(dataStore[0])
+        }
+    });
+    
+    for(const element of dataStore) {
+        var row = table.insertRow(-1);
+        var food = row.insertCell(0);
+        var calories = row.insertCell(1);
+        var carbs = row.insertCell(2);
+        var fats =  row.insertCell(3);
+        var proteins =  row.insertCell(4);
+        var button = row.insertCell(5);
+        
+        food.innerHTML = element.name ;
+        calories.innerHTML = element.total_calories + " Kcals";
+        carbs.innerHTML = element.total_carbs + " g";
+        fats.innerHTML = element.total_fat +" g";
+        proteins.innerHTML = element.total_protein + " g";
+        button.innerHTML = '<button>Add</button>';
+    }
+}
 
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
