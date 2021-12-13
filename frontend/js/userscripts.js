55308277e61d0eb17672256db635c1b281aa4af6
dfinn417 <dfinn02@nyit.edu>
2021-12-13 00:40:12 -0500

    help

---
+++ frontend/js/userscripts.js
@@ -0,0 +1,21 @@
+function getName() {
+    var name;
+    var userData;
+    
+    $.ajax({
+        "url": "http://journey.gadget.sh:8080/user",
+        "method": "GET",
+        "timeout": 0,
+        "async": false,
+        "headers": {
+        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDE5NjE0MzYsInN1YiI6IjYxYjZiNzIxOTg4ODk0NTQ2ZDk1YmZmNyIsInRpZCI6IjEifQ.vO7NgkrxTs-Bzcwf8DLVfjVnWQt3O1umsl5UUgpTazs"
+        },
+        "success": function(data){
+            name = data.fullname;
+            //console.log(name);
+        }
+    });
+    
+    console.log(name);
+    document.getElementById("usersName").innerHTML = name
+}
